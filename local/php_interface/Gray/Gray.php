f0112ce259bf9f8630e44845f00608ece9d183d5
sergej <dekke48st@gmail.com>
2024-02-19 16:25:15 +0300

    2

---
+++ local/php_interface/Gray/Gray.php
@@ -0,0 +1,8 @@
+<?php
+class Gray
+{
+    public static function show($var)
+    {
+        echo "<pre>". var_dump($var) ."</pre>";
+    }
+}
--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,1 +1,4 @@
 <?php
+if (file_exists($_SERVER['DOCUMENT_ROOT']. "/local/php_interface/Gray/Gray.php")) {
+   require $_SERVER['DOCUMENT_ROOT']."/local/php_interface/Gray/Gray.php";
+}

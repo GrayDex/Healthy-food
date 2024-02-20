a28ba2b1883bbc3b252c004c5471aec6738f9484
sergej <dekke48st@gmail.com>
2024-02-20 17:58:58 +0300

    Template4_1

--- index.php
+++ index.php
@@ -1,2 +1,5 @@
-<?php require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php"); ?>
-<?php require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
+<?php require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+$APPLICATION->SetTitle('Main site');
+var_dump(__FILE__);
+?>
+<?php require_once($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
--- local/templates/main/header.php
+++ local/templates/main/header.php
@@ -1,25 +1,18 @@
 <?php
-if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die; ?>
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die;
+?>
+
 <!doctype html>
-<html lang="ru">
+<html lang=<?=LANGUAGE_ID?>>
 
 <head>
-    <?php
-    if(file_exists($_SERVER['DOCUMENT_ROOT'] . "/local/templates/main/include/templ/assets.php")) {
-        require_once($_SERVER['DOCUMENT_ROOT'] . "/local/templates/main/include/templ/assets.php");
-    } ?>
-
+    <?php require_once($_SERVER['DOCUMENT_ROOT'] . SITE_TEMPLATE_PATH."/include/templ/assets.php"); ?>
     <?php $APPLICATION->ShowHead(); ?>
     <title><?php $APPLICATION->ShowTitle(); ?></title>
 </head>
 
 <body class="body">
-<div><?php $APPLICATION->ShowPanel() ?></div>
-<div class="wrapper">
-
-    <?php
-    if(file_exists($_SERVER['DOCUMENT_ROOT'] . "/local/templates/main/include/templ/header.php")) {
-        require_once($_SERVER['DOCUMENT_ROOT'] . "/local/templates/main/include/templ/header.php");
-    } ?>
-
-    <main class="main">
+    <div><?php $APPLICATION->ShowPanel() ?></div>
+        <div class="wrapper">
+            <?php require_once($_SERVER['DOCUMENT_ROOT'] . SITE_TEMPLATE_PATH."/include/templ/header.php"); ?>
+                <main class="main">

f3a0f1428b2e919d8eacf5380e60417f941908be
sergej <dekke48st@gmail.com>
2024-02-20 16:32:31 +0300

    Template4

--- local/templates/main/footer.php
+++ local/templates/main/footer.php
@@ -1,5 +1,8 @@
 </main>
-<?php require($_SERVER['DOCUMENT_ROOT'] . "/local/templates/main/include/templ/footer.php"); ?>
+<?php
+if(file_exists($_SERVER['DOCUMENT_ROOT'] . "/local/templates/main/include/templ/footer.php")) {
+    require_once($_SERVER['DOCUMENT_ROOT'] . "/local/templates/main/include/templ/footer.php");
+} ?>
 		</div>
 		<script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU&amp;apikey=<ваш API-ключ>"></script>
 	</body>
--- local/templates/main/header.php
+++ local/templates/main/header.php
@@ -4,7 +4,11 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die; ?>
 <html lang="ru">
 
 <head>
-    <?php require($_SERVER['DOCUMENT_ROOT'] . "/local/templates/main/include/templ/assets.php"); ?>
+    <?php
+    if(file_exists($_SERVER['DOCUMENT_ROOT'] . "/local/templates/main/include/templ/assets.php")) {
+        require_once($_SERVER['DOCUMENT_ROOT'] . "/local/templates/main/include/templ/assets.php");
+    } ?>
+
     <?php $APPLICATION->ShowHead(); ?>
     <title><?php $APPLICATION->ShowTitle(); ?></title>
 </head>
@@ -12,5 +16,10 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die; ?>
 <body class="body">
 <div><?php $APPLICATION->ShowPanel() ?></div>
 <div class="wrapper">
-    <?php require($_SERVER['DOCUMENT_ROOT'] . "/local/templates/main/include/templ/header.php"); ?>
+
+    <?php
+    if(file_exists($_SERVER['DOCUMENT_ROOT'] . "/local/templates/main/include/templ/header.php")) {
+        require_once($_SERVER['DOCUMENT_ROOT'] . "/local/templates/main/include/templ/header.php");
+    } ?>
+
     <main class="main">

b1807b55e83f7687eaeb16c169c5e3e5d4c0fe58
GrayDex <dekker48st@gmail.com>
2024-02-24 13:58:22 +0300

    template_refactor

--- local/templates/main/include/areas/footer/content__rect-copy.php
+++
@@ -1,4 +0,0 @@
-<?php if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die; ?>
-
-
-<div class="footer-content__rect-copy">© 2008-2024 «Здоровые продукты»</div>
--- local/templates/main/include/areas/header/main__contacts-col right-col.php
+++
@@ -1,9 +0,0 @@
-<?php if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die; ?>
-
-<div class="header-main__contacts-col right-col">
-    <div class="header-main__contacts-top"><a class="header-main__contacts-policy" href="#">Политика конфиденциальности</a></div>
-    <div class="header-main__contacts-bot">
-        <div class="header-main__contacts-copy">© 2008-2023 «Здоровые продукты»</div>
-        <div class="header-main__contacts-dev"><span>Сделано в</span><a href="#" target="blank"> Клаудмил</a></div>
-    </div>
-</div>
